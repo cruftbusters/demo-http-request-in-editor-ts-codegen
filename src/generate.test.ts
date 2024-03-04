9ee9b575c6ea2f983e4d24e0ac5cec856f451a0d
tyler <tyler@cruftbusters.com>
2024-03-03 17:49:35 -0800

    generate bindings for simple named get and post with url

--- src/generate.test.ts
+++ src/generate.test.ts
@@ -1,9 +1,65 @@
 let generate = require('./generate')
+let trim_indent = require('./trim_indent')
 
 test('default', () => {
     expect(generate(``)).toBe(`module.exports = {}`)
 })
 
 test('request without name', () => {
-    expect(generate(`GET http://google.com`)).toBe(`module.exports = {}`)
+    expect(generate(`GET https://google.com`)).toBe(`module.exports = {}`)
+})
+
+test('trailing name', () => {
+    let actual = generate(trim_indent(`
+    GET https://google.com
+    # name : getGoogle
+    `));
+
+    let expected = trim_indent(`
+    module.exports = {}
+    module.exports.getGoogle = () => axios.get('https://google.com')
+    `);
+
+    expect(actual).toBe(expected)
+})
+
+test('get url', () => {
+    let actual = generate(trim_indent(`
+    # name : getGoogle
+    GET https://google.com
+    `));
+
+    let expected = trim_indent(`
+    module.exports = {}
+    module.exports.getGoogle = () => axios.get('https://google.com')
+    `);
+
+    expect(actual).toBe(expected)
+})
+
+
+test('post url', () => {
+    let actual = generate(trim_indent(`
+    # name : postGoogle
+    POST https://google.com
+    `));
+
+    let expected = trim_indent(`
+    module.exports = {}
+    module.exports.postGoogle = () => axios.post('https://google.com')
+    `);
+
+    expect(actual).toBe(expected)
+
+    let actual_1 = generate(trim_indent(`
+    GET http://google.com
+    # name : getGoogle
+    `));
+
+    let expected_1 = trim_indent(`
+    module.exports = {}
+    module.exports.getGoogle = () => axios.get('http://google.com')
+    `);
+
+    expect(actual_1).toBe(expected_1)
 })
--- src/generate.ts
+++ src/generate.ts
@@ -1,5 +1,40 @@
+interface Binding {
+    name: string
+    verb: string
+    url: string
+}
+
 function generate(string: string) {
-    return 'module.exports = {}'
+    let result = ['module.exports = {}']
+    let maybe_request = to_maybe_request(string.split('\n'))
+    if (maybe_request.length > 0) {
+        let [request] = maybe_request
+        result.push(to_typescript(request))
+    }
+    return result.join('\n')
+}
+
+function to_maybe_request(lines: string[]) {
+    let request: Binding = {
+        name: '',
+        verb: '',
+        url: ''
+    }
+    for (let line of lines) {
+        if (line.startsWith('# name : ')) {
+            request.name = line.split(' ')[3]
+        }
+        if (line.startsWith('GET ') || line.startsWith('POST ')) {
+            let parts = line.split(' ')
+            request.verb = parts[0].toLowerCase()
+            request.url = parts[1]
+        }
+    }
+    return request.name ? [request] : []
+}
+
+function to_typescript({name, verb, url}: Binding) {
+    return `module.exports.${name} = () => axios.${verb}('${url}')`
 }
 
 module.exports = generate
